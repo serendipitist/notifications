e105a40d178830118d65a22d2f1e39df4923dc31
serendipitist <serendipitist@github.com>
2018-05-15 17:53:04 +0530

    react, webpack setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import style from "./Search.css"
+
+const Index = () => {
+  return <div>hello</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
